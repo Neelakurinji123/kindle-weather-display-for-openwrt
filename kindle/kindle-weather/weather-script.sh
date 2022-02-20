e4814e5e7b7a98eadbd8a94726087c883858def8
Neelakurinji123 <70471447+Neelakurinji123@users.noreply.github.com>
2022-02-20 21:47:33 +0900

    Update weather-script.sh

--- kindle/kindle-weather/weather-script.sh
+++ kindle/kindle-weather/weather-script.sh
@@ -4,10 +4,18 @@ cd "$(dirname "$0")"
 
 if [ -f /mnt/us/kindle-weather/enable ]; then
 
-    [ -n "$(pidof powerd)" ] && (/etc/init.d/powerd stop; /etc/init.d/framework stop)
+    if [ "$(pidof powerd)" != '' ]; then
+        /etc/init.d/powerd stop
+        /etc/init.d/framework stop
+    fi
 
-    rm -f /tmp/KindleStation_flatten.png
-    scp -i /mnt/us/.ssh/id_rsa root@192.168.2.1:/tmp/KindleStation_flatten.png /tmp
+    if ! $(mount -l | grep -q 'tmpfs on /tmp_data'); then
+        mount -a
+        sleep 3
+    fi
+
+    rm -f /www/KindleStation_flatten.png
+    scp -i /mnt/us/.ssh/id_rsa root@192.168.2.1:/tmp/KindleStation_flatten.png /tmp_data/
     eips -c
-    eips -g /tmp/KindleStation_flatten.png
+    eips -g /tmp_data/KindleStation_flatten.png
 fi
